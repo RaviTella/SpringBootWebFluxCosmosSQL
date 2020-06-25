6a09bc2ec317c3733b1b5e3af70c29f9b25f1797
Ravi Tella <RaviTella@users.noreply.github.com>
2020-06-24 21:45:39 -0500

    refactored database conenctivity logic into a new class CosmosDB

---
+++ ReadingListWebApp/src/main/java/com/ratella/ReactiveReadingListWebApp/model/CosmosDB.java
@@ -0,0 +1,84 @@
+package com.ratella.ReactiveReadingListWebApp.model;
+
+import com.azure.cosmos.*;
+import com.azure.cosmos.models.CosmosContainerProperties;
+import com.azure.cosmos.models.ThroughputProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class CosmosDB {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private String endpoint;
+    private String key;
+    private String databaseName;
+    private String containerName;
+    private List<String> locations;
+    private CosmosAsyncClient client;
+    private CosmosAsyncDatabase database;
+    private CosmosAsyncContainer container;
+
+    public CosmosDB(@Value("${database.endpoint}") final String endpoint, @Value("${database.key}") final String key,
+                    @Value("${database.databaseName}") final String databaseName, @Value("${database.containerName}") final String containerName,
+                    @Value("#{'${database.locations}'.split(',')}") final List<String> locations) {
+        this.endpoint = endpoint;
+        this.key = key;
+        this.databaseName = databaseName;
+        this.containerName = containerName;
+        this.locations = locations;
+        cosmosSetup();
+    }
+
+
+    private void cosmosSetup() {
+        CosmosContainerProperties containerProperties = new CosmosContainerProperties(containerName, "/reader");
+        buildAndGetClient()
+                .createDatabaseIfNotExists(databaseName)
+                .flatMap(databaseResponse -> {
+                    database = client.getDatabase(databaseResponse
+                            .getProperties()
+                            .getId());
+                    return database
+                            .createContainerIfNotExists(containerProperties, ThroughputProperties.createManualThroughput(400));
+                })
+                .flatMap(containerResponse -> {
+                    container = database.getContainer(containerResponse
+                            .getProperties()
+                            .getId());
+                    return Mono.empty();
+                })
+                .subscribeOn(Schedulers.elastic())
+                .block();
+    }
+
+
+    private CosmosAsyncClient buildAndGetClient() {
+        if (client == null) {
+            logger.info(endpoint);
+            client = new CosmosClientBuilder()
+                    .endpoint(endpoint)
+                    .key(key)
+                    .preferredRegions(locations)
+                    .contentResponseOnWriteEnabled(true)
+                    .consistencyLevel(ConsistencyLevel.SESSION)
+                    .buildAsyncClient();
+            return client;
+        }
+        return client;
+    }
+
+    public CosmosAsyncContainer getContainer() {
+        return container;
+    }
+
+
+}
--- ReadingListWebApp/src/main/java/com/ratella/ReactiveReadingListWebApp/model/CosmosRepository.java
+++ ReadingListWebApp/src/main/java/com/ratella/ReactiveReadingListWebApp/model/CosmosRepository.java
@@ -3,11 +3,12 @@ package com.ratella.ReactiveReadingListWebApp.model;
 import com.azure.cosmos.*;
 import com.azure.cosmos.models.*;
 import com.azure.cosmos.util.CosmosPagedFlux;
-import com.sun.xml.internal.bind.v2.TODO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -16,29 +17,21 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 
-@Component
+@Service
 public class CosmosRepository {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Value("${database.endpoint}")
-    private String endpoint;
-    @Value("${database.key}")
-    private String key;
-    @Value("${database.databaseName}")
-    private String databaseName;
-    @Value("${database.containerName}")
-    private String containerName;
-    @Value("#{'${database.locations}'.split(',')}")
-    private List<String> locations;
-    private CosmosAsyncClient client;
-    private CosmosAsyncDatabase database;
-    private CosmosAsyncContainer container;
+    private CosmosDB cosmosDB;
 
+    @Autowired
+   public CosmosRepository(CosmosDB cosmosDB){
+        this.cosmosDB = cosmosDB;
+    }
 
     public Flux<Book> getReadingList(String reader) {
         CosmosQueryRequestOptions queryOptions = new CosmosQueryRequestOptions();
         queryOptions.setMaxBufferedItemCount(10);
         String query = "SELECT * FROM ReadingList r WHERE r.reader = " + "'" + reader + "'";
-        CosmosPagedFlux<Book> pagedFluxResponse = container.queryItems(
+        CosmosPagedFlux<Book> pagedFluxResponse = cosmosDB.getContainer().queryItems(
                 query, queryOptions, Book.class);
         return pagedFluxResponse
                 .byPage()
@@ -46,7 +39,7 @@ public class CosmosRepository {
     }
 
     public Mono<Book> createBook(Book book) {
-        return container
+        return cosmosDB.getContainer()
                 .createItem(book)
                 .map(cosmosItemResponse -> {
                     return cosmosItemResponse.getItem();
@@ -55,64 +48,23 @@ public class CosmosRepository {
     }
 
     public Mono<Book> updateBook(Book book) {
-        return container
+        return cosmosDB.getContainer()
                 .replaceItem(book, book.getId(), new PartitionKey(book.getReader()))
                 .map(CosmosItemResponse::getItem);
     }
 
     public Mono<Book> findBookByID(String id, String partitionKey) {
-        return container
+        return cosmosDB.getContainer()
                 .readItem(id, new PartitionKey(partitionKey), Book.class)
                 .map(CosmosItemResponse::getItem);
     }
 
     public Mono<CosmosItemResponse<Object>> deleteBookByID(String id, String partitionKey) {
-        return container
+        return cosmosDB.getContainer()
                 .deleteItem(id, new PartitionKey(partitionKey));
         //TODO Why is getItem() returning null?
         // .map(CosmosAsyncItemResponse::getItem);
 
     }
-
-
-    @PostConstruct
-    private void cosmosSetup() {
-        CosmosContainerProperties containerProperties = new CosmosContainerProperties(containerName, "/reader");
-        getClient()
-                .createDatabaseIfNotExists(databaseName)
-                .flatMap(databaseResponse -> {
-                    database = getClient().getDatabase(databaseResponse
-                            .getProperties()
-                            .getId());
-                    return database
-                            .createContainerIfNotExists(containerProperties, ThroughputProperties.createManualThroughput(400));
-                })
-                .flatMap(containerResponse -> {
-                    container = database.getContainer(containerResponse
-                            .getProperties()
-                            .getId());
-                    return Mono.empty();
-                })
-                .subscribeOn(Schedulers.elastic())
-                .subscribe();
-    }
-
-
-    private CosmosAsyncClient getClient() {
-        if (client == null) {
-            logger.info(endpoint);
-            client = new CosmosClientBuilder()
-                    .endpoint(endpoint)
-                    .key(key)
-                    .preferredRegions(locations)
-                    .contentResponseOnWriteEnabled(true)
-                    .consistencyLevel(ConsistencyLevel.SESSION)
-                    .buildAsyncClient();
-            return client;
-        }
-        return client;
-
-    }
-
 
 }
